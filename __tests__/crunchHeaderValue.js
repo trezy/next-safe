db9106ef7b8f9e92e3a550c8c65e3c98ec08baa5
Trezy <tre@trezy.com>
2021-01-05 15:04:14 -0600

    test(crunchheadervalue): add tests

---
+++ __tests__/crunchHeaderValue.js
@@ -0,0 +1,48 @@
+// Local imports
+import { crunchHeaderValue } from '../lib/crunchHeaderValue'
+
+
+
+
+
+describe('crunchHeaderValue', () => {
+	test('with single header', () => {
+		const crunchedHeaders = crunchHeaderValue({
+			'header-name': 'header-value',
+		})
+
+		expect(crunchedHeaders).toEqual('header-name header-value;')
+	})
+
+	test('with multiple headers', () => {
+		const crunchedHeaders = crunchHeaderValue({
+			'header-name1': 'header-value1',
+			'header-name2': 'header-value2',
+		})
+
+		expect(crunchedHeaders).toEqual('header-name1 header-value1;header-name2 header-value2;')
+	})
+
+	test('with values array', () => {
+		const crunchedHeaders = crunchHeaderValue({
+			'header-name': [
+				'header-value1',
+				'header-value2',
+			],
+		})
+
+		expect(crunchedHeaders).toEqual('header-name header-value1 header-value2;')
+	})
+
+	test('with mixed value types', () => {
+		const crunchedHeaders = crunchHeaderValue({
+			'header-name1': 'header-value1',
+			'header-name2': [
+				'header-value2',
+				'header-value3',
+			],
+		})
+
+		expect(crunchedHeaders).toEqual('header-name1 header-value1;header-name2 header-value2 header-value3;')
+	})
+})
