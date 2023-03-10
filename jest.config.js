7a535fea734a48fc4b941634897277e06a9cfa4a
Sam Bauers <sam@wopr.com.au>
2023-03-10 18:23:07 +1100

    build(jest.config.js): add ts-check and types

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,26 @@
-/*
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/en/configuration.html
- */
+// @ts-check
 
-module.exports = {
-	preset: 'ts-jest',
+/**
+ * @type {import('jest').Config}
+ **/
+const jestConfig = {
+  preset: 'ts-jest',
 
-	// Automatically clear mock calls and instances between every test
-	clearMocks: true,
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
 
-	// The directory where Jest should output its coverage files
-	coverageDirectory: 'coverage',
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
 
-	// An array of directory names to be searched recursively up from the requiring module's location
-	moduleDirectories: ['node_modules', 'shared'],
+  // An array of directory names to be searched recursively up from the requiring module's location
+  moduleDirectories: ['node_modules', 'shared'],
 
-	// An array of file extensions your modules use
-	moduleFileExtensions: ['ts', 'js'],
+  // An array of file extensions your modules use
+  moduleFileExtensions: ['ts', 'js'],
 
-	// A list of paths to modules that run some code to configure or set up the testing framework before each test
-	setupFilesAfterEnv: [],
+  // A list of paths to modules that run some code to configure or set up the testing framework before each test
+  setupFilesAfterEnv: [],
 }
+
+// eslint-disable-next-line no-undef
+module.exports = jestConfig
